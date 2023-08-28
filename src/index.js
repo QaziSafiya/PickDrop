ffd3fc2c37831b8371c73343f40a63fd4bb3824a
safiya siddiqui <safiyaqazi517@gmail.com>
2023-08-28 22:26:28 +0530

    pic&drop

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Nav from './Nav';
-import Home from './Home';
-import Analytics from './Analytics';
-import Trending from './Trending';
-import Referal from './Referal';
-import Footer from './Footer';
-import Triks from './Triks';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Nav from "./Nav";
+import Home from "./Home";
+import Analytics from "./Analytics";
+import Trending from "./Trending";
+import Referal from "./Referal";
+import Footer from "./Footer";
+import Triks from "./Triks";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Nav/>
-    <Home/>
-    <Analytics/>
- <Trending/>
- <Referal/>
- <Triks/>
- <Footer/>
- 
+    <Nav />
+    <Home />
+    <Analytics />
+    <Trending />
+    <Referal />
+    <Triks />
+    <Footer />
+    {/* hello */}
   </React.StrictMode>
 );
-
+// If you want to start measuring performance in your app, pass a function
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
